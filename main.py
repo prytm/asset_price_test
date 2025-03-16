178c142be26cfc0eec22022fc1e922bfd229a634
prytm <125776274+prytm@users.noreply.github.com>
2025-03-16 11:16:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import seaborn as sns
 #######################
 # Page configuration
 st.set_page_config(
-    page_title="Risk Projection",
+    page_title="Black Scholes Pricing Asset",
     page_icon="📉",
     layout="wide",
     initial_sidebar_state="expanded")
@@ -60,78 +60,28 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # (Include the BlackScholes class definition here)
+def BlackScholes (S, K, T, r, sigma):
+    d1 = (np.log(S/K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
 
-class BlackScholes:
-    def __init__(
-        self,
-        time_to_maturity: float,
-        strike: float,
-        current_price: float,
-        volatility: float,
-        interest_rate: float,
-    ):
-        self.time_to_maturity = time_to_maturity
-        self.strike = strike
-        self.current_price = current_price
-        self.volatility = volatility
-        self.interest_rate = interest_rate
+    call_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    put_price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
 
-    def calculate_prices(
-        self,
-    ):
-        time_to_maturity = self.time_to_maturity
-        strike = self.strike
-        current_price = self.current_price
-        volatility = self.volatility
-        interest_rate = self.interest_rate
-
-        d1 = (
-            log(current_price / strike) +
-            (interest_rate + 0.5 * volatility ** 2) * time_to_maturity
-            ) / (
-                volatility * sqrt(time_to_maturity)
-            )
-        d2 = d1 - volatility * sqrt(time_to_maturity)
-
-        call_price = current_price * norm.cdf(d1) - (
-            strike * exp(-(interest_rate * time_to_maturity)) * norm.cdf(d2)
-        )
-        put_price = (
-            strike * exp(-(interest_rate * time_to_maturity)) * norm.cdf(-d2)
-        ) - current_price * norm.cdf(-d1)
-
-        self.call_price = call_price
-        self.put_price = put_price
-
-        # GREEKS
-        # Delta
-        self.call_delta = norm.cdf(d1)
-        self.put_delta = 1 - norm.cdf(d1)
-
-        # Gamma
-        self.call_gamma = norm.pdf(d1) / (
-            strike * volatility * sqrt(time_to_maturity)
-        )
-        self.put_gamma = self.call_gamma
-
-        return call_price, put_price
-
-# Function to generate heatmaps
-# ... your existing imports and BlackScholes class definition ...
-
+    return call_price, put_price
 
 # Sidebar for User Inputs
 with st.sidebar:
     st.title("📊 Black-Scholes Model")
-    st.write("`Created by:`")
+    st.write("Created by:")
     linkedin_url = "https://www.linkedin.com/in/prytm/"
     st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Priya Tammam`</a>', unsafe_allow_html=True)
 
+    stocks = st.text_input("Underlying Asset", value = "AAPL")
     current_price = st.number_input("Current Asset Price", value=100.0)
     strike = st.number_input("Strike Price", value=100.0)
     time_to_maturity = st.number_input("Time to Maturity (Years)", value=1.0)
-    volatility = st.number_input("Volatility (σ)", value=0.2)
     interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
+    volatility = df['Close'].std()
 
     st.markdown("---")
     calculate_btn = st.button('Heatmap Parameters')
@@ -143,41 +93,53 @@ with st.sidebar:
     spot_range = np.linspace(spot_min, spot_max, 10)
     vol_range = np.linspace(vol_min, vol_max, 10)
 
+# Stocks df
+end = dt.datetime.now()
+start = dt.datetime(end.year - 1, end.month, end.day)
 
+df = yf.download(stocks, start, end)
+df.columns = df.columns.get_level_values(0)
 
-def plot_heatmap(bs_model, spot_range, vol_range, strike):
-    call_prices = np.zeros((len(vol_range), len(spot_range)))
-    put_prices = np.zeros((len(vol_range), len(spot_range)))
-    
-    for i, vol in enumerate(vol_range):
-        for j, spot in enumerate(spot_range):
-            bs_temp = BlackScholes(
-                time_to_maturity=bs_model.time_to_maturity,
-                strike=strike,
-                current_price=spot,
-                volatility=vol,
-                interest_rate=bs_model.interest_rate
-            )
-            bs_temp.calculate_prices()
-            call_prices[i, j] = bs_temp.call_price
-            put_prices[i, j] = bs_temp.put_price
-    
-    # Plotting Call Price Heatmap
-    fig_call, ax_call = plt.subplots(figsize=(10, 8))
-    sns.heatmap(call_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_call)
-    ax_call.set_title('CALL')
-    ax_call.set_xlabel('Spot Price')
-    ax_call.set_ylabel('Volatility')
-    
-    # Plotting Put Price Heatmap
-    fig_put, ax_put = plt.subplots(figsize=(10, 8))
-    sns.heatmap(put_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_put)
-    ax_put.set_title('PUT')
-    ax_put.set_xlabel('Spot Price')
-    ax_put.set_ylabel('Volatility')
-    
-    return fig_call, fig_put
+# Candle Stick PLot
+def plot_candlestick_volume(df, ticker='CBA'):
+    fig = make_subplots(
+        rows=2, cols=1, shared_xaxes=True,
+        vertical_spacing=0.1,
+        subplot_titles=(f'{ticker} Price Chart', 'Volume'),
+        row_width=[0.2, 0.7]
+    )
 
+    # Candlestick
+    fig.add_trace(go.Candlestick(
+        x=df.index,
+        open=df['Open'],
+        high=df['High'],
+        low=df['Low'],
+        close=df['Close'],
+        name='OHLC'
+    ), row=1, col=1)
+
+    # Moving Averages
+    fig.add_trace(go.Scatter(x=df.index, y=df['MA50'], line=dict(color='blue', width=1), name='MA50'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df.index, y=df['MA200'], line=dict(color='grey', width=1), name='MA200'), row=1, col=1)
+
+    # Volume
+    fig.add_trace(go.Bar(x=df.index, y=df['Volume'], marker_color='red', showlegend=False), row=2, col=1)
+
+    fig.update_layout(
+        title=f'{ticker} Historical Price Chart',
+        xaxis_tickfont_size=10,
+        yaxis=dict(title='Price ($/Share)'),
+        autosize=True,
+        height=700,
+        margin=dict(l=50, r=50, b=100, t=100, pad=4),
+        paper_bgcolor='black',
+        plot_bgcolor='black',
+        font=dict(color='white')
+    )
+
+    fig.update(layout_xaxis_rangeslider_visible=False)
+    return fig
 
 # Main Page for Output Display
 st.title("Black-Scholes Pricing Model")
@@ -194,8 +156,7 @@ input_df = pd.DataFrame(input_data)
 st.table(input_df)
 
 # Calculate Call and Put values
-bs_model = BlackScholes(time_to_maturity, strike, current_price, volatility, interest_rate)
-call_price, put_price = bs_model.calculate_prices()
+call_price, put_price = BlackScholes(strike, time_to_maturity, interest_rate, volatility)
 
 # Display Call and Put Values in colored tables
 col1, col2 = st.columns([1,1], gap="small")
@@ -205,7 +166,7 @@ with col1:
     st.markdown(f"""
         <div class="metric-container metric-call">
             <div>
-                <div class="metric-label">CALL Value</div>
+                <div class="metric-label">Optimal CALL Value</div>
                 <div class="metric-value">${call_price:.2f}</div>
             </div>
         </div>
@@ -216,27 +177,24 @@ with col2:
     st.markdown(f"""
         <div class="metric-container metric-put">
             <div>
-                <div class="metric-label">PUT Value</div>
+                <div class="metric-label">Optimal PUT Value</div>
                 <div class="metric-value">${put_price:.2f}</div>
             </div>
         </div>
     """, unsafe_allow_html=True)
 
 st.markdown("")
-st.markdown("")
-st.markdown("")
-st.title("Options Price - Interactive Heatmap")
+st.title("Options Price - Interactive Candlestick Chart")
 st.info("Explore how option prices fluctuate with varying 'Spot Prices and Volatility' levels using interactive heatmap parameters, all while maintaining a constant 'Strike Price'.")
 
 # Interactive Sliders and Heatmaps for Call and Put Options
-col1, col2 = st.columns([1,1], gap="small")
+if not df.empty:
+    df['MA50'] = df['Close'].rolling(window=ma50_window, min_periods = 0).mean()
+    df['MA200'] = df['Close'].rolling(window=ma200_window, min_periods = 0).mean()
 
-with col1:
-    st.subheader("Call Price Heatmap")
-    heatmap_fig_call, _ = plot_heatmap(bs_model, spot_range, vol_range, strike)
-    st.pyplot(heatmap_fig_call)
-
-with col2:
-    st.subheader("Put Price Heatmap")
-    _, heatmap_fig_put = plot_heatmap(bs_model, spot_range, vol_range, strike)
-    st.pyplot(heatmap_fig_put)
+    # Call plot function
+    st.subheader(f"{ticker} Candlestick Chart")
+    candlestick_fig = plot_candlestick_volume(df, ticker)
+    st.plotly_chart(candlestick_fig, use_container_width=True)
+else:
+    st.warning("⚠️ No data available for the selected ticker and date range.")

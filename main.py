7d300f2f66332dc700fb74e2cbee521c8cb79e3d
prytm <125776274+prytm@users.noreply.github.com>
2025-03-17 09:44:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ with st.sidebar:
         """)
         
     with st.expander("📘 What is an Option?"):
-        st.text("""
+        st.markdown("""
         **Options** are financial derivatives that give the buyer the **right**, but not the obligation, to **buy (Call)** or **sell (Put)** an underlying asset at a specified price (strike price) before or at a certain date.
         
         - **Call Option** → Gives the right to **buy** the asset → used when you expect the price to **go up**.
@@ -112,7 +112,7 @@ with st.sidebar:
         
         ---
         **Example**  
-        Suppose AAPL stock is at $213. You buy a Call Option with a strike price of $200. If AAPL rises to $230, you can buy it for $200 — earning a $30 profit (minus premium).
+        Suppose AAPL stock is at $213. You buy a Call Option with a strike price of **USD 200**. If AAPL rises to **USD 230**, you can buy it for **USD 200** — earning a **USD 30** profit (minus premium).
         """)
         
     with st.expander("📊 What are Greeks in Options?"):
